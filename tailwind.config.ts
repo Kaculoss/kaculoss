f2bacb1e3e063d06a11b2be583c34b817e175a4b
Kaculoss <benjerosani99@gmail.com>
2023-10-02 22:02:28 +0000

    updated tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,39 @@ const config: Config = {
         primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
+        background: "#11998e",
+        foreground: "hsl(213 31% 91%)",
+        destructive: {
+          DEFAULT: "hsl(0 63% 31%)",
+          foreground: "hsl(210 40% 98%)",
+        },
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "scroll-left": {
+          from: { transform: "translateX(80%)" },
+          to: { transform: "translateX(calc(-100% - 1.5rem))" },
+        },
+        "scroll-left-1": {
+          from: { transform: "translateX(20%)" },
+          to: { transform: "translateX(calc(-100% - 1.5rem))" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll-left": "scroll-left 30s linear infinite alternate",
+        "scroll-left-1": "scroll-left-1 45s linear infinite alternate",
       },
     },
   },
