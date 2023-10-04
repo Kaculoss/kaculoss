fc1a90b827d2af5683a68a8d802c4ddbfb9420a2
Kaculoss <benjerosani99@gmail.com>
2023-10-04 02:08:11 +0000

    updated

--- components/Works.tsx
+++ components/Works.tsx
@@ -40,7 +40,10 @@ const ProjectCard = ({
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className="relative w-full h-[230px]">
+        <div
+          className="relative w-full h-[230px]"
+          onClick={() => window.open(link, "_blank")}
+        >
           <Image
             src={image}
             alt="project_image"
